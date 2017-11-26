d926bec18673b905110a892eebad921b0ebcea34
Mahfuz Kabir <mahfuzk@gmail.com>
2017-11-26 07:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   var result = [];
   var str = ''
-  for (i = 0; i < musicians.length; i++) {
-    str = `${musicians[i]} ${instruments[i]}` 
+  for (var i = 0; i < musicians.length; i++) {
+    str = `${musicians[i]} plays ${instruments[i]}` 
     result.push(str);
   }
   return result;
